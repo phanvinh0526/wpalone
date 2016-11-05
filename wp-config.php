72163a8cd73853bab64c7d374d14123b69e7b666
Duy Ho <duy_hotan2000@yahoo.com>
2016-11-06 01:07:51 +0700

    Database configuration

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define('DB_NAME', 'wpalone');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wpalone');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'wpalone');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '188.166.217.91');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
